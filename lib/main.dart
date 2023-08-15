c93ebab6269782237b4117d8a37b718688916799
Roman Kozak <cossack.roman@gmail.com>
2023-08-15 17:51:48 +0300

    ui, bugfixing

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
 import 'dart:async';
+import 'dart:ui';
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:web_socket_channel/io.dart';
 import 'dart:io' show Platform;
-import 'package:window_size/window_size.dart';
-// import 'package:window_manager/window_manager.dart';
+// import 'package:window_size/window_size.dart';
+import 'package:flutter_chat_bubble/chat_bubble.dart';
+
+import 'messages.dart';
+// /import 'package:window_manager/window_manager.dart';
 // import 'package:flutter_acrylic/flutter_acrylic.dart';
-// import 'package:bitsdojo_window/bitsdojo_window.dart';
+// import 'package:desktop_window/desktop_window.dart';
+import 'package:flutter/material.dart';
+// import 'dart:io' show Platform;
+// import 'dart:math' as math;
+// import 'package:window_size/window_size.dart' as window_size;
 // import 'package:flutter_acrylic/widgets/visual_effect_subview_container/visual_effect_subview_container.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';..
 
 MaterialColor createMaterialColor(Color color) {
   List strengths = <double>[.05];
@@ -16,68 +25,113 @@ MaterialColor createMaterialColor(Color color) {
   for (int i = 1; i < 10; i++) {
     strengths.add(0.1 * i);
   }
-  strengths.forEach((strength) {
+  for (var strength in strengths) {
     final double ds = 0.5 - strength;
-    swatch[(strength * 1000).round()] = Color.fromRGBO( r + ((ds < 0 ? r : (255 - r)) * ds).round(), g + ((ds < 0 ? g : (255 - g)) * ds).round(), b + ((ds < 0 ? b : (255 - b)) * ds).round(), 1, ); });
+    swatch[(strength * 1000).round()] = Color.fromRGBO(
+      r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+      g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+      b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+      1,
+    );
+  }
   return MaterialColor(color.value, swatch);
 }
 
 // const borderColor = Color(0xFF805306);
-var mainColor = Color(0xFF1b390e);
-var darkColor = Color(0xFF102208);
+var mainColor = const Color(0xFF1b390e);
+var darkColor = const Color(0xFF102208);
 var fontColor = Colors.green.withAlpha(200);
 // var mainColor = Color(0xFF38761d);
 //
 //1b390e
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
-    setWindowTitle('Robin AI chat');
-    const minmaxWidth = 500.0;
-    setWindowMaxSize(const Size(minmaxWidth, 800));
-    setWindowMinSize(const Size(minmaxWidth, 600));
-  }
+  // if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+  // setWindowTitle('Robin AI chat');
+  // const minMaxWidth = 450.0;
+  // setWindowMaxSize(const Size(minMaxWidth, 700));
+  // setWindowMinSize(const Size(minMaxWidth, 560));
+  // }
+  // await windowManager.ensureInitialized();
+  // WindowOptions windowOptions = const WindowOptions(
+  //   size: Size(450, 700),
+  //   center: true,
+  //   backgroundColor: Colors.transparent,
+  //   skipTaskbar: false,
+  //   // titleBarStyle: TitleBarStyle.hidden,
+  // );
+
+  // // var screen_width = WidgetsBinding.instance.platformDispatcher.views.first.physicalSize.width;
+  // windowManager.waitUntilReadyToShow(windowOptions, () async {
+  //   await windowManager.show();
+  //   await windowManager.focus();
+  //   // var o = await windowManager.getPosition();
+  //
+  // });
   // await Window.initialize();
+  // windowManager.setAlignment(Alignment(500.0, 500.0));
+  // windowManager.setPosition(const Offset(0, 0));
+  // var window = await window_size.getWindowInfo();
+  // if (window.screen != null) {
+  //   final screenFrame = window.screen?.visibleFrame;
+  //   final width = 400.0;
+  //   final height = 600.0;
+  //   final left = ((screenFrame!.width - width) / 2).roundToDouble();
+  //   final top = ((screenFrame.height - height) / 3).roundToDouble();
+  //   final frame = Rect.fromLTWH(left, top, width, height);
+  //   window_size.setWindowFrame(frame);
+  //   window_size.setWindowMinSize(Size(1.0 * width, 1.0 * height));
+  //   window_size.setWindowMaxSize(Size(1.0 * width, 1.0 * height));
+  // }
   runApp(
     MaterialApp(
-        theme: ThemeData( primarySwatch: createMaterialColor(mainColor), ),
-        home: MyApp()
-    ),
+        theme: ThemeData(
+          primarySwatch: createMaterialColor(mainColor),
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const MyApp()),
   );
 
-  // doWhenWindowReady(() {
-    // const initialSize = Size(600, 450);
-    // appWindow.minSize = initialSize;
-    // appWindow.size = initialSize;
-    // appWindow.alignment = Alignment.center;
-    // appWindow.show();
-  //   final win = appWindow;
-  //   const initialSize = Size(500, 450);
-  //   win.minSize = initialSize;
-  //   win.size = initialSize;
-  //   win.alignment = Alignment.centerLeft;
-  //   win.title = "Custom window with Flutter";
-  //   win.show();
-  // });
+  // FlutterView view = PlatformDispatcher.instance.views.first;
+  // double physicalWidth = view.physicalSize.width;
+  // double physicalHeight = view.physicalSize.height;
+  // double devicePixelRatio = view.devicePixelRatio;
+  // double screenWidth = physicalWidth / devicePixelRatio;
+  // double screenHeight = physicalHeight / devicePixelRatio;
+
+  // var screen_width = WidgetsBinding.instance.platformDispatcher.views.first.physicalSize.width;
+  // Size screenSize = WidgetsBinding.instance.window.physicalSize;
+  // var screen_width = screenSize.width;
+  doWhenWindowReady(() {
+    final win = appWindow;
+    const initialSize = Size(400, 450);
+    win.minSize = initialSize;
+    win.size = initialSize;
+    win.alignment = Alignment.bottomRight;
+    var left = 1550.0;
+    var top = 300.0;
+    win.position = Offset(left, top);
+    win.title = "Custom window with Flutter";
+    // win .position(Offset(500,500));
+    win.show();
+    win.position = Offset(left, top);
+  });
 
   // await Window.setEffect(
   //   effect: WindowEffect.transparent, color: Color(0xCC222222),
   // );
   // windowManager.waitUntilReadyToShow().then((_) async{
-  //   // await windowManager.setTitleBarStyle(TitleBarStyle.hidden);
-  //   // await windowManager.setAsFrameless();
+  // await windowManager.setAlignment(Alignment(500.0, 500.0));
+  // await windowManager.setTitleBarStyle(TitleBarStyle.hidden);
+  // await windowManager.setAsFrameless();
+  // await windowManager.setPosition(const Offset(560, 500));
   // });
 }
 
-class ChatMessage {
-  String messageContent;
-  String messageType;
-
-  ChatMessage({required this.messageContent, required this.messageType});
-}
-
 class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
   @override
   _MyAppState createState() => _MyAppState();
 }
@@ -98,10 +152,7 @@ class _MyAppState extends State<MyApp> {
     super.initState();
   }
 
-// This is what you're looking for!
   void _scrollDown() {
-    // Timer(Duration(milliseconds: 500), () { _controller.jumpTo(_controller.position.maxScrollExtent); });
-    // _controller.jumpTo(_controller.position.maxScrollExtent*1.8);
     if (_controller.hasClients) {
       Future.delayed(const Duration(milliseconds: 200)).then((value) {
         _controller.animateTo(
@@ -111,48 +162,29 @@ class _MyAppState extends State<MyApp> {
         );
       });
     }
-    // _controller.animateTo(
-    //   _controller.position.maxScrollExtent,
-    //   duration: Duration(seconds: 2),
-    //   curve: Curves.fastOutSlowIn,
-    // );
   }
 
   // Localhost for android - 10.0.2.2
   // Localhost for iOS - 127.0.0.1
   final IOWebSocketChannel channel =
       IOWebSocketChannel.connect(Uri.parse('ws://127.0.0.1:8765'));
-  //IOWebSocketChannel.connect( Uri.parse('ws://192.168.0.104:6785'));
-  // connect('ws://127.0.0.1:6785');
 
   var userInput;
   List<ChatMessage> messages = <ChatMessage>[];
 
-  // @override
-  // void initState(){
-  //   super.initState();
-  //   channel.stream.listen((message){
-  //     print(message);
-  //     setState(() { messages.add(message); });
-  //   },
-  //   onDone: () {
-  //
-  //   },
-  // );}
-
+  @override
   void dispose() {
     channel.sink.close();
     super.dispose();
   }
 
   send2WebSocket() {
-    // print("send to websocket: $userInput");
     channel.sink.add(userInput);
   }
 
   void submitMessage(String value) {
     setState(() {
-       messages.add(ChatMessage(messageContent: value, messageType: 'me'));
+      messages.add(ChatMessage(messageContent: value, messageType: 'me'));
     });
     userInput = value;
     myFocusNode.requestFocus();
@@ -164,65 +196,119 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   centerTitle: true,
-      //   title: const Text("Robin Chat"),
-      // ),
       backgroundColor: Colors.black,
       body: Column(
         children: <Widget>[
+          WindowTitleBarBox(
+            child: Row(
+              children: [Expanded(child: MoveWindow()), const WindowButtons()],
+            ),
+          ),
           Expanded(
             child: StreamBuilder(
               stream: channel.stream,
               builder: (context, snapshot) {
-                // The data is being pulled from the websocket
-                // _scrollDown();
-                  if (!snapshot.hasError && snapshot.connectionState == ConnectionState.active && snapshot.hasData) {
-                    // print(snapshot.data);
-                    if (messages.length > 1) {
-                      if (messages.elementAt(messages.length - 2).messageContent != snapshot.data) {
-                        messages.add(ChatMessage(
-                            messageContent: snapshot.data,
-                            messageType: 'receiver'));
-                      }}
-                    else
+                if (!snapshot.hasError &&
+                    snapshot.connectionState == ConnectionState.active &&
+                    snapshot.hasData) {
+                  if (messages.length > 1) {
+                    if (messages
+                            .elementAt(messages.length - 2)
+                            .messageContent !=
+                        snapshot.data) {
                       messages.add(ChatMessage(
                           messageContent: snapshot.data,
                           messageType: 'receiver'));
+                    }
+                  } else {
+                    messages.add(ChatMessage(
+                        messageContent: snapshot.data,
+                        messageType: 'receiver'));
+                  }
+                  _scrollDown();
+                  // windowManager.setPosition(Offset(500.0, 500.0));
                   return ListView.builder(
                       controller: _controller,
-                      // reverse: true,
                       shrinkWrap: true,
-                      padding: const EdgeInsets.only(top: 10, bottom: 10),
+                      padding: const EdgeInsets.only(
+                          top: 10, bottom: 10, left: 25, right: 25),
                       itemCount: messages.length,
                       itemBuilder: (context, index) {
-                        return Container(
-                          padding: const EdgeInsets.only(
-                              left: 16, right: 16, top: 10, bottom: 10),
-                          child: Align(
-                            alignment: messages[index].messageType == "receiver"
-                                ? Alignment.topLeft
-                                : Alignment.topRight,
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(20),
-                                  color:
-                                      messages[index].messageType == "receiver"
-                                          ? darkColor
-                                          : fontColor.withAlpha(350)),
-                              padding: const EdgeInsets.all(25),
-                              child: Text(
-                                messages[index].messageContent,
-                                style: TextStyle(
-                                    fontSize: 16,
-                                    color: messages[index].messageType == "receiver"
-                                        ? fontColor
-                                        : fontColor.withAlpha(250)
+                        return messages[index].messageType == 'receiver'
+                            ? ChatBubble(
+                                clipper: ChatBubbleClipper5(
+                                    type: BubbleType.sendBubble),
+                                alignment: Alignment.topRight,
+                                margin: EdgeInsets.only(top: 20),
+                                backGroundColor: mainColor,
+                                child: Container(
+                                  constraints: BoxConstraints(
+                                    maxWidth:
+                                        MediaQuery.of(context).size.width * 0.7,
+                                  ),
+                                  child: Text(
+                                    messages[index].messageContent,
+                                    style: TextStyle(
+                                        fontSize: 15, color: fontColor),
+                                  ),
                                 ),
-                              ),
-                            ),
-                          ),
-                        );
+                              )
+                            : ChatBubble(
+                                clipper: ChatBubbleClipper5(
+                                    type: BubbleType.receiverBubble),
+                                alignment: Alignment.topLeft,
+                                margin: EdgeInsets.only(top: 20),
+                                backGroundColor: darkColor,
+                                child: Container(
+                                  constraints: BoxConstraints(
+                                    maxWidth:
+                                        MediaQuery.of(context).size.width * 0.7,
+                                  ),
+                                  child: Text(
+                                    messages[index].messageContent,
+                                    style: TextStyle(color: fontColor),
+                                  ),
+                                ),
+                              );
+                        // Container(
+                        //   padding: const EdgeInsets.only(
+                        //       left: 16, right: 16, top: 10, bottom: 10),
+                        //   child: Align(
+                        //     alignment: messages[index].messageType == "receiver"
+                        //         ? Alignment.topLeft
+                        //         : Alignment.topRight,
+                        //     child: Container(
+                        //       decoration: BoxDecoration(
+                        //         borderRadius: BorderRadius.circular(20),
+                        //         color: messages[index].messageType == "receiver"
+                        //             ? darkColor
+                        //             : Colors.black,
+                        //         // fontColor.withAlpha(350)
+                        //         border:
+                        //             messages[index].messageType == "receiver"
+                        //                 ? Border.all(
+                        //                     color: Colors.blueAccent,
+                        //                     style: BorderStyle.none,
+                        //                   )
+                        //                 : Border.all(
+                        //                     color: mainColor,
+                        //                     style: BorderStyle.solid,
+                        //                     width: 2,
+                        //                   ),
+                        //       ),
+                        //       padding: const EdgeInsets.all(25),
+                        //       child: Text(
+                        //
+                        //         style: TextStyle(
+                        //             fontSize: 16,
+                        //             color: messages[index].messageType ==
+                        //                     "receiver"
+                        //                 ? fontColor
+                        //                 : fontColor.withAlpha(250)),
+                        //       ),
+                        //     ),
+                        //   ),
+                        // );
                       });
                 } else {
                   return const Center(
@@ -250,8 +336,6 @@ class _MyAppState extends State<MyApp> {
                     hintStyle: TextStyle(color: Colors.grey[700]),
                     hintText: "Write message...",
                   ),
-                  // hintStyle: TextStyle(color: Colors.black54),
-                  // border: InputBorder.none),
                   onChanged: (value) => userInput = value,
                   onSubmitted: (value) => submitMessage(value),
                   textInputAction: TextInputAction.go,
@@ -277,8 +361,58 @@ class _MyAppState extends State<MyApp> {
               ),
             ],
           ),
+          const SizedBox(
+            width: 20,
+          ),
         ],
       ),
+    );
+  }
+}
+
+final buttonColors = WindowButtonColors(
+    iconNormal: const Color(0xFF805306),
+    mouseOver: mainColor,
+    mouseDown: const Color(0xFF805306),
+    iconMouseOver: const Color(0xFF805306),
+    iconMouseDown: mainColor);
+
+final closeButtonColors = WindowButtonColors(
+    mouseOver: darkColor,
+    mouseDown: darkColor,
+    iconNormal: const Color(0xFF805306),
+    iconMouseOver: Colors.white);
+
+class WindowButtons extends StatefulWidget {
+  const WindowButtons({Key? key}) : super(key: key);
+
+  @override
+  _WindowButtonsState createState() => _WindowButtonsState();
+}
+
+class _WindowButtonsState extends State<WindowButtons> {
+  void maximizeOrRestore() {
+    setState(() {
+      appWindow.maximizeOrRestore();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        MinimizeWindowButton(colors: buttonColors),
+        appWindow.isMaximized
+            ? RestoreWindowButton(
+                colors: buttonColors,
+                onPressed: maximizeOrRestore,
+              )
+            : MaximizeWindowButton(
+                colors: buttonColors,
+                onPressed: maximizeOrRestore,
+              ),
+        CloseWindowButton(colors: closeButtonColors),
+      ],
     );
   }
 }
